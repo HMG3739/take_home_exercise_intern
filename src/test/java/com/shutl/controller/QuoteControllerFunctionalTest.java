08c8492c0460c217584a3183fec09554b97fc47a
HMG3739 <74410947+HMG3739@users.noreply.github.com>
2022-05-26 14:08:10 +0100

    Update QuoteControllerFunctionalTest.java

--- src/test/java/com/shutl/controller/QuoteControllerFunctionalTest.java
+++ src/test/java/com/shutl/controller/QuoteControllerFunctionalTest.java
@@ -1,40 +1,39 @@
 package com.shutl.controller;
-
+​
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.shutl.Application;
 import com.shutl.model.Quote;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.web.context.WebApplicationContext;
-
+​
 import static org.junit.Assert.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
-
+​
 @WebAppConfiguration
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = Application.class)
+@SpringBootTest
 public class QuoteControllerFunctionalTest {
-
+​
     @Autowired private WebApplicationContext webApplicationContext;
-
+​
     ObjectMapper objectMapper = new ObjectMapper();
-
+​
     private MockMvc mockMvc;
-
+​
     @Before
     public void setup() {
         this.mockMvc = webAppContextSetup(this.webApplicationContext).build();
     }
-
+​
     @Test
     public void testBasicService() throws Exception {
         Quote quoteData = new Quote("SW1A1AA", "EC2A3LT");
@@ -43,13 +42,13 @@ public class QuoteControllerFunctionalTest {
                 .content(objectMapper.writeValueAsString(quoteData)))
             .andExpect(status().isOk())
             .andReturn();
-
+​
         Quote quote = objectMapper.readValue(result.getResponse().getContentAsString(), Quote.class);
         assertEquals(quote.getPickupPostcode(), "SW1A1AA");
         assertEquals(quote.getDeliveryPostcode(), "EC2A3LT");
-        assertEquals(quote.getPrice(), new Long(316));
+        assertEquals(quote.getPrice(), Long.valueOf(316));
     }
-
+​
     @Test
     public void testVariablePricingByDistance() throws Exception {
         Quote quoteData = new Quote("SW1A1AA", "EC2A3LT");
@@ -58,22 +57,22 @@ public class QuoteControllerFunctionalTest {
                 .content(objectMapper.writeValueAsString(quoteData)))
             .andExpect(status().isOk())
             .andReturn();
-
+​
         Quote quote = objectMapper.readValue(result.getResponse().getContentAsString(), Quote.class);
         assertEquals(quote.getPickupPostcode(), "SW1A1AA");
         assertEquals(quote.getDeliveryPostcode(), "EC2A3LT");
-        assertEquals(quote.getPrice(), new Long(316));
-
+        assertEquals(quote.getPrice(), Long.valueOf(316));
+​
         quoteData = new Quote("AL15WD", "EC2A3LT");
         result = this.mockMvc.perform(post("/quote")
                 .contentType("application/json")
                 .content(objectMapper.writeValueAsString(quoteData)))
             .andExpect(status().isOk())
             .andReturn();
-
+​
         quote = objectMapper.readValue(result.getResponse().getContentAsString(), Quote.class);
         assertEquals(quote.getPickupPostcode(), "AL15WD");
         assertEquals(quote.getDeliveryPostcode(), "EC2A3LT");
-        assertEquals(quote.getPrice(), new Long(305));
+        assertEquals(quote.getPrice(), Long.valueOf(305));
     }
 }
